cf17a7abaef496d390dbd3124566562274b59188
Marco Rodrigues <marcoaurelio853@gmail.com>
2022-12-15 18:48:04 +0000

    export supabase

--- src/supabase.js
+++ src/supabase.js
@@ -4,3 +4,5 @@ const supabaseUrl = 'https://fcajkqpptvlwnvnubizh.supabase.co';
 const supabaseKey =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZjYWprcXBwdHZsd252bnViaXpoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzA4NTM4NjEsImV4cCI6MTk4NjQyOTg2MX0.nXyNoTBhR5ll6OmEaSgb6GGHdTW8_mpr0Mq0Ka9121k';
 const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
